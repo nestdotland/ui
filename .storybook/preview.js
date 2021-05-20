60a89101ae79f0bde6a3a7bc650f3b3d535bc91a
Maximous Black <maximousblk@gmail.com>
2021-05-20 08:38:30 +0000

    update storybook backgrounds

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,7 +8,6 @@ export const parameters = {
     theme: NestTheme,
   },
   backgrounds: {
-    default: 'twitter',
     values: [
       {
         name: 'gray/900',
@@ -17,6 +16,38 @@ export const parameters = {
       {
         name: 'gray/800',
         value: tailwindColors.coolGray[800],
+      },
+      {
+        name: 'gray/700',
+        value: tailwindColors.coolGray[700],
+      },
+      {
+        name: 'gray/600',
+        value: tailwindColors.coolGray[600],
+      },
+      {
+        name: 'gray/500',
+        value: tailwindColors.coolGray[500],
+      },
+      {
+        name: 'gray/400',
+        value: tailwindColors.coolGray[400],
+      },
+      {
+        name: 'gray/300',
+        value: tailwindColors.coolGray[300],
+      },
+      {
+        name: 'gray/200',
+        value: tailwindColors.coolGray[200],
+      },
+      {
+        name: 'gray/100',
+        value: tailwindColors.coolGray[100],
+      },
+      {
+        name: 'gray/050',
+        value: tailwindColors.coolGray[50],
       },
     ],
   },
